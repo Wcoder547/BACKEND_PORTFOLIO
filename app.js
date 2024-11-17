40886c3fd49870bb583c6bbb2973d0018f661ef8
Wcoder547 <mw5667155@gmail.com>
2024-11-17 14:39:02 +0500

    cors error:

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ import projectRouter from "./router/projectRouter.js";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
-const allowedOrigins = [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL];
+const allowedOrigins = ["*"];
 
 app.use(
   cors({
@@ -28,6 +28,8 @@ app.use(
     credentials: true,
   })
 );
+console.log("Portfolio URL:", process.env.PORTFOLIO_URL);
+console.log("Dashboard URL:", process.env.DASHBOARD_URL);
 
 app.use(cookieParser());
 app.use(express.json());
