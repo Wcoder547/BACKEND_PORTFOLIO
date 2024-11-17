64054b6ee82b0948832b4a0e5d2f756de6720fe6
Wcoder547 <mw5667155@gmail.com>
2024-11-17 13:44:42 +0500

    latest commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,6 +7,8 @@ export const generateToken = (user, message, statuscode, res) => {
         Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: "None",
+      secure: true,
     })
     .json({
       success: true,
